b0990678ed7a3e1ed6094ab53d81ce641e139c9b
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-10-11 20:08:53 -0300

    Removing unused import

--- src/models/configs.py
+++ src/models/configs.py
@@ -1,6 +1,5 @@
 from flask_restx import fields
 from src.program.server import server
-from src.models.id import id
 
 types_response = server.api.model('Units', {
     'unidades': fields.List(fields.String),
