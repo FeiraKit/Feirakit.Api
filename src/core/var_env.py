1dcce44cff7aec2cc48240bd2840d81aeb9dee27
Henrique Barroso <barrosohenriquelima@gmail.com>
2024-02-14 10:46:43 -0300

    config prod envs

--- src/core/var_env.py
+++ src/core/var_env.py
@@ -1,9 +1,9 @@
-from dotenv import load_dotenv, find_dotenv
+# from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
 class VarEnvs():
     def __init__(self):
-        load_dotenv(find_dotenv())
+        # load_dotenv(find_dotenv())
         self.secret_key = getenv("SECRET_KEY")
         self.database = getenv("DATABASE")
         self.db_connection = getenv("DB_CONNECTION")
